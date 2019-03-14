41f41305e8cf7c1390eb8e2787c6e20420adf7c1
Gonçalo Almeida <32540757+xaleza@users.noreply.github.com>
2019-03-14 20:35:10 +0000

    input fucking feito

--- Projeto.java
+++ Projeto.java
@@ -1,55 +1,68 @@
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.StreamTokenizer;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class Projeto{
 
-    public static void dfs(int i, int[][] graph, boolean[] visited, ArrayList<Integer> nodes) {
+    public static void dfs(int i, int[][] graph, boolean[] visited, ArrayList<Integer> subgraph) {
         if(!visited[i]){        
             visited[i] = true; // Mark node as "visited"
-            nodes.add(i+1);
+            subgraph.add(i+1);
     
             for (int j = 0; j < graph[i].length; j++) {
                 if (graph[i][j]==1 && !visited[j]) {   
-                    dfs(j, graph, visited, nodes); // Visit node
+                    dfs(j, graph, visited, subgraph); // Visit node
                 }
             }
         }   
     }
     public static void main(String[] args){
         
-        String filename = args[0];
         int numOfRouters = 0;
-        int numOfCon = 0;
-        int x;
+        int numOfCon = 0; //numero de conexoes
+        
+        int x; //var aux da matriz
         int y;
+
+        StreamTokenizer st = new StreamTokenizer(new BufferedReader(new InputStreamReader(System.in)));
+        st.parseNumbers();
+
         try{
-            BufferedReader in = new BufferedReader(new FileReader(filename));
-            String s = new String();
-
-            numOfRouters = Integer.parseInt(in.readLine());
-            numOfCon = Integer.parseInt(in.readLine());
+            st.nextToken();
+            numOfRouters = (int) st.nval;
             int[][] matrix = new int[numOfRouters][numOfRouters];
-
-            while((s = in.readLine()) != null){
-                String[] fields = s.split(" ");
-                x = Integer.parseInt(fields[0]) - 1;
-                y = Integer.parseInt(fields[1]) - 1;
-                matrix[x][y] = 1;
-                matrix[y][x] = 1;
+            st.nextToken();
+            numOfCon = (int) st.nval;
+            
+            for(int i = 0; i < numOfCon; i++){
+                st.nextToken();
+                x = (int) st.nval -1;
+                st.nextToken();
+                y = (int) st.nval -1;
+                matrix[x][y]= 1;
+                matrix[y][x]= 1;
             }
+            System.out.println(numOfRouters);
+            System.out.println(numOfCon);
 
+            for(int i = 0; i< numOfRouters; i++){
+                for(int j = 0; j<numOfRouters; j++)
+                    System.out.print(matrix[i][j]);
+                System.out.println();
+            }
+            
             boolean [] visited = new boolean[numOfRouters];
-            ArrayList<Integer> nodes = new ArrayList<Integer>();
-            ArrayList<Integer> r = new ArrayList<Integer>();
+            ArrayList<Integer> subgraph = new ArrayList<Integer>(); //para colocar os elementos de cada subrede
+            ArrayList<Integer> r = new ArrayList<Integer>(); //R (maiores indentificadores de cada subrede)
             int count = 0;
             for(int i = 0; i < numOfRouters; i++) {
                 if(!visited[i]) {
-                    dfs(i,matrix,visited,nodes);
-                    r.add(Collections.max(nodes));
-                    nodes = new ArrayList<Integer>();
+                    dfs(i,matrix,visited,subgraph);
+                    r.add(Collections.max(subgraph));
+                    subgraph = new ArrayList<Integer>();
                     ++count;
                 }
             }
@@ -58,17 +71,6 @@ public class Projeto{
                 System.out.print(r.get(i) + " ");
             System.out.println();
             
-
-
-            // System.out.println("numOfRouters = " + numOfRouters);
-            // System.out.println("numOfCon = " + numOfCon);
-            // for(int i = 0; i < numOfRouters; i++){
-            //     for(int j = 0; j < numOfRouters; j++){
-            //         d = matrix[i][j];
-            //         System.out.print(d +" ");
-            //     }
-            //     System.out.print("\n");
-            // }
         }
         catch (IOException e){}
         //catch (FileNotFoundException f){}
